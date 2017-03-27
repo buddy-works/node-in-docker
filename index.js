d3f7e6a352d9ab1ab3a0f9b5a375764d4c1b4535
Aleksander Kuś <Kivlov84@users.noreply.github.com>
2017-03-27 14:25:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-//Za pomoc¹ directive require ³adujemy (load) modu³ express
+//Load express module with `require` directive
 var express = require('express')
 var app = express()
 
 
-//definiujemy response dla requestów w root URL (/)
+//Define request response in root URL (/)
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 
-//aplikacja uruchamia serwer s³uchaj¹cy na porcie 8081
+//Launch listening server on port 8081
 app.listen(8081, function () {
   console.log('app listening on port 8081!')
 })
